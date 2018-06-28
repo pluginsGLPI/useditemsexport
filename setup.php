fbfb0b28071e6b8537c530894b60f6a26343a0a1
Cédric Anne <cedric.anne@gmail.com>
2018-06-28 13:42:42 +0200

    Fix version compatibility check for GLPI < 9.2

--- setup.php
+++ setup.php
@@ -102,6 +102,25 @@ function plugin_version_useditemsexport() {
  * @return boolean
  */
 function plugin_useditemsexport_check_prerequisites() {
+
+   //Version check is not done by core in GLPI < 9.2 but has to be delegated to core in GLPI >= 9.2.
+   $version = preg_replace('/^((\d+\.?)+).*$/', '$1', GLPI_VERSION);
+   if (version_compare($version, '9.2', '<')) {
+      $matchMinGlpiReq = version_compare($version, PLUGIN_USEDITEMSEXPORT_MIN_GLPI, '>=');
+      $matchMaxGlpiReq = version_compare($version, PLUGIN_USEDITEMSEXPORT_MAX_GLPI, '<');
+
+      if (!$matchMinGlpiReq || !$matchMaxGlpiReq) {
+         echo vsprintf(
+            'This plugin requires GLPI >= %1$s and < %2$s.',
+            [
+               PLUGIN_USEDITEMSEXPORT_MIN_GLPI,
+               PLUGIN_USEDITEMSEXPORT_MAX_GLPI,
+            ]
+         );
+         return false;
+      }
+   }
+
    return true;
 }
 
