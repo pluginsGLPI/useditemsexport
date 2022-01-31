1a536da1f3037e44b9ef39381f559c361bbc7bd1
Cédric Anne <cedric.anne@gmail.com>
2022-01-31 15:31:09 +0100

    Fix licence headers

--- front/export.form.php
+++ front/export.form.php
@@ -1,25 +1,31 @@
 <?php
+
 /**
- * --------------------------------------------------------------------------
+ * -------------------------------------------------------------------------
+ * UsedItemsExport plugin for GLPI
+ * -------------------------------------------------------------------------
+ *
  * LICENSE
  *
- * This file is part of useditemsexport.
+ * This file is part of UsedItemsExport.
  *
- * useditemsexport is free software; you can redistribute it and/or modify
+ * UsedItemsExport is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- * useditemsexport is distributed in the hope that it will be useful,
+ * UsedItemsExport is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * --------------------------------------------------------------------------
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with UsedItemsExport. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
  * @author    François Legastelois
- * @copyright Copyright © 2015 - 2018 Teclib'
- * @license   AGPLv3+ http://www.gnu.org/licenses/agpl.txt
+ * @copyright Copyright (C) 2016-2022 by UsedItemsExport plugin team.
+ * @license   AGPLv3 https://www.gnu.org/licenses/agpl-3.0.html
  * @link      https://github.com/pluginsGLPI/useditemsexport
- * @link      https://pluginsglpi.github.io/useditemsexport/
  * -------------------------------------------------------------------------
  */
 
