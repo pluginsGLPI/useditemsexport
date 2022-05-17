b9517d3e225243bb69f8219f39a3e77cadb7fa22
Cédric Anne <cedric.anne@gmail.com>
2022-05-17 15:15:59 +0200

    2.5.1

--- setup.php
+++ setup.php
@@ -30,7 +30,7 @@
  */
 
 // Plugin version
-define("PLUGIN_USEDITEMSEXPORT_VERSION", "2.5.0");
+define("PLUGIN_USEDITEMSEXPORT_VERSION", "2.5.1");
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_USEDITEMSEXPORT_MIN_GLPI", "10.0.1");
