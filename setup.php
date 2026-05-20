1efa831a7f3678603a85c603a60263878b5aa0e7
Lauris-oss <laurynas.paunksnis@idvilnius.lt>
2026-05-20 09:15:50 +0300

    Add files via upload
    Signed-off-by: Lauris-oss <laurynas.paunksnis@idvilnius.lt>

--- setup.php
+++ setup.php
@@ -34,7 +34,7 @@ use Glpi\Plugin\Hooks;
 use function Safe\define;
 
 // Plugin version - bumped to trigger migration for configurable report fields
-define('PLUGIN_USEDITEMSEXPORT_VERSION', '2.7.0');
+define('PLUGIN_USEDITEMSEXPORT_VERSION', '2.7.1');
 define('PLUGIN_USEDITEMSEXPORT_MIN_GLPI', '11.0.0');
 define('PLUGIN_USEDITEMSEXPORT_MAX_GLPI', '11.0.99');
 
